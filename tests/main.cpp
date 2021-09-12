50611e9834f9ed410e2d2509592a422877609fb6
Haris Gušić <harisgusic.dev@gmail.com>
2021-09-13 00:07:30 +0200

    Add test for processLine

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,10 +6,29 @@
 #include <QFile>
 #include <iostream>
 
+extern QString processLine(QString, const Device&);
+
 class TestWrite : public QObject
 {
     Q_OBJECT
 private slots:
+    void testProcessLine() {
+        Device device;
+        device.setName("MyDevice");
+        device.setModel("MyModel");
+        QCOMPARE(processLine("class $name", device), "class MyDevice");
+        QCOMPARE(processLine("class $name : $model", device), "class MyDevice : MyModel");
+        QCOMPARE(processLine("string \"$name\"", device), "string \"MyDevice\"");
+        QCOMPARE(processLine("string \"$name\" aftertext $model", device),
+                 "string \"MyDevice\" aftertext MyModel");
+
+        // Degenerate cases
+        QCOMPARE(processLine("", device), ""); // empty string
+        QCOMPARE(processLine("$", device), "$");
+        QCOMPARE(processLine("$nonexistent", device), "");
+        QCOMPARE(processLine("$nonexistent $model", device), " MyModel");
+    }
+
     void testBasicWrite()
     {
         // Populate the device with data
@@ -22,7 +41,7 @@ private slots:
         // Write the file
         QDir("_out").removeRecursively();
         QDir().mkpath("_out");
-        write(device, "files/basic.h.in", "_out/basic.h");
+        writeDevice(device, "files/basic.h.in", "_out/basic.h");
 
         // Compare the output file with expected output
         QFile file1("_out/basic.h"), file2("files/basic.h");
